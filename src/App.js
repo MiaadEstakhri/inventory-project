29652379c5153422026cd6e7008dbe3eb976696c
MiaadEstakhri <miaad8104@gmail.com>
2022-11-16 19:55:34 +0330

    adding NavBar component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import NavBar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <section className="bg-slate-800 min-h-screen">
+        {/* app bar */}
+        <NavBar />
+      </section>
     </div>
   );
 }
